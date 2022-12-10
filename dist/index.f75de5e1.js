76f7c3bd58651e244a84f368d3fd272798b3310b
kluyko-pavel <kluyko.pasha@mail.ru>
2022-12-10 17:02:29 +0300

    added  adaptive, modifi menu-burger

--- dist/index.f75de5e1.js
+++ dist/index.f75de5e1.js
@@ -2,7 +2,40 @@ new Swiper(".swiper", {
     pagination: {
         el: ".swiper-pagination",
         clickable: true
+    },
+    spaceBetween: 16,
+    breakpoints: {
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 32
+        }
     }
 });
+new Swiper(".reviews__content", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    spaceBetween: 16,
+    breakpoints: {
+        1024: {
+            slidesPerView: 1.5,
+            spaceBetween: 32,
+            pagination: {
+                el: null
+            },
+            navigation: {
+                nextEl: ".swiper-button-next"
+            }
+        }
+    }
+});
+const menuButton = document.querySelector(".menu-burger");
+const menuBox = document.querySelector(".main-header__menu-box");
+const toggleMenuButton = ()=>{
+    menuButton.classList.toggle("active");
+    menuBox.classList.toggle("active");
+};
+menuButton.addEventListener("click", toggleMenuButton);
 
 //# sourceMappingURL=index.f75de5e1.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,4 +3,39 @@ new Swiper(".swiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  spaceBetween: 16,
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+  },
 });
+new Swiper(".reviews__content", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  spaceBetween: 16,
+  breakpoints: {
+    1024: {
+      slidesPerView: 1.5,
+      spaceBetween: 32,
+      pagination: {
+        el: null,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+      },
+    },
+  },
+});
+const menuButton = document.querySelector(".menu-burger");
+const menuBox = document.querySelector(".main-header__menu-box");
+
+const toggleMenuButton = () => {
+  menuButton.classList.toggle("active");
+  menuBox.classList.toggle("active");
+};
+
+menuButton.addEventListener("click", toggleMenuButton);
